6b5ce6c975846285ee56abd6eeb4243f94f699e4
Zach K <huntingmanzach@gmail.com>
2023-10-29 06:23:09 -0400

    Update bot and pehkui file

--- flow-bot/flow-bot.py
+++ flow-bot/flow-bot.py
@@ -1,3 +1,5 @@
+# this file is the flow bot, in all of it's code.
+
 from interactions import Client, Intents, listen
 from interactions import slash_command, slash_option, SlashContext, OptionType
 from interactions import Button, ButtonStyle, ActionRow
@@ -11,6 +13,7 @@ import httpx
 
 import asyncio
 import re
+from os.path import exists
 
 UNSAFE_CHARS = re.compile(r"[^a-zA-Z0-9_-]+")
 IMG_LINKS = re.compile(r"!\[(.*?)\]\((.*?)\)")
@@ -21,20 +24,20 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 
-
 bot = Client(
     intents = Intents.ALL,
     token = config['bot']['TOKEN'],
     default_scope = config['bot']['GUILD'] 
 )
 
-
-
+sessions = {}
 flow_data = {}
 async def load_data():
+    global flow_data, meta_map, meta_start
     repo = config['bot']['REPO']
     online = config['bot']['ONLINE']
 
+    # Download content
     async with httpx.AsyncClient() as http_client:
         resp = await http_client.get(repo + "index.yaml")
         index = yaml.safe_load(resp.content)
@@ -48,57 +51,121 @@ async def load_data():
             for file in index['data']:
                 tg.create_task(load_file(file))
     
+    # Parse links
     for page in flow_data.values():
         text = page["text"].strip()
-        text = IMG_LINKS.sub(rf"[\1](<{repo}i/\2.png>)", text)
+        text = IMG_LINKS.sub(rf"[IMAGE: \1](<{repo}i/\2.png>)", text)
         text = SITE_LINKS.sub(rf"](<{online}\1>)", text)
         text = REG_LINKS.sub(rf"](<\1>)", text)
         page["text"] = text
 
-async def make_flow_message(message: interactions.Message, link: str = "index"):
-    l = link[1:] if link[0] == "#" else link
-    l = UNSAFE_CHARS.sub("", l)
-    page = flow_data.get(l, flow_data["default"])
-    
-    short: bool = "short" in page
-    text: str = "## " + page["title"] + "\n" + page.get("short", page["text"])
-    if len(text) > 1950:
-        space = max(text.rfind(" ", 0, 1850), text.rfind("\t", 0, 1850), text.rfind("\r", 0, 1850), text.rfind("\n", 0, 1850))
-        if space < 500: space = 1850
-        text = text[:space] + "..."
-        short = True
-    
-    components = []
-    for opt in page.get("opts", []):
-        components.append(Button(style=ButtonStyle.GRAY, label=opt["text"], custom_id=opt["link"]))
-    
-    if len(components) > 25:
-        end = components[-1]
-        components = components[:23]
-        components.append(end)
-        components.append(Button(style=ButtonStyle.BLUE, label="More online...", custom_id="more-online"))
-        short = True
+class Session:
+    def __init__(self, ctx: SlashContext, message: interactions.Message):
+        self.ctx = ctx
+        self.message = message
+        self.age = 0
 
-    if short:
-        text += f"\n\nTo read more, see the [#{l}](<{config['bot']['ONLINE']}#{l}>) page online."
+        self.buttons = []
+        self.start = 0
+        self.prev = []
+        self.link = ""
     
-    await message.edit(content=text, components=ActionRow.split_components(*components) if components else [])
+    async def load(self, link: str):
+        l = link.strip()
+        l = l[1:] if l[0] == "#" else l
+        l = UNSAFE_CHARS.sub("", l)
+
+        if self.link:
+            self.prev.append(self.link)
+        self.link = l
+        page = flow_data.get(l, flow_data["default"])
+        
+        short: bool = "short" in page
+        text: str = f"## {page['title']} ({self.link})\n{page.get('short', page['text'])}"
+        if len(text) > 1950:
+            space = max(text.rfind(" ", 0, 1850), text.rfind("\t", 0, 1850), text.rfind("\r", 0, 1850), text.rfind("\n", 0, 1850))
+            if space < 500: space = 1850
+            text = text[:space] + "..."
+            short = True
+        
+        if short:
+            text += f"\n\nTo read more, see the [#{l}](<{config['bot']['ONLINE']}#{l}>) page online."
+        
+        self.buttons = []
+        for opt in page.get("opts", []):
+            self.buttons.append(Button(style=ButtonStyle.GRAY, label=opt["text"], custom_id=opt["link"]))
+        components = self.buttons
+
+        m = 24 if self.prev else 25
+        if len(components) > m:
+            if self.start > len(components): self.start = 0
+
+            end = min(self.start+m-1, len(components))
+            components = components[self.start:end]
+            components.append(Button(style=ButtonStyle.BLUE, label="More...", custom_id="more-button"))
+        
+        if self.prev:
+            components.append(Button(style=ButtonStyle.RED, label="Back", custom_id="back-button"))
+        
+        await self.message.edit(content=text, components=ActionRow.split_components(*components) if components else [])
+
+    async def back(self):
+        if not self.prev: return
+        self.link = ""
+        await self.load(self.prev.pop())
+    
+    async def more(self):
+        self.link = ""
+        self.start += 23 if self.prev else 24
+        await self.load(self.link) # Not technically necessary, but I'm lazy
+    
+    async def close(self):
+        if self.buttons:
+            await self.message.edit(components=[])
+            del sessions[self.message.id]
 
 @listen(Component)
 async def on_component(event: Component):
     ctx = event.ctx
+    if ctx.message is None: return
+
+    message = ctx.message
+
     await ctx.defer(edit_origin=True)
 
-    if ctx.custom_id == "more-online":
-        pass
-    elif ctx.message is not None:
-        await make_flow_message(ctx.message, ctx.custom_id)
+    s = sessions.get(message.id)
+    if s is None:
+        # Broken state, typically caused by improper shutdown.
+        await message.edit(components=[])
+        return
     
+    match ctx.custom_id:
+        case 'more-button':
+            await s.more()
+        case 'back-button':
+            await s.back()
+        case link:
+            await s.load(link)
 
+async def close_sessions():
+    for s in sessions.values():
+        await s.close()
+    
+async def timeout_sessions():
+    delay = int(config['session']['AGE_TIMER'])
+    max_age = int(config['session']['MAX_AGE'])
+    while True:
+        await asyncio.sleep(delay)
+        for s in list(sessions.values()):
+            s.age += 1
+            if s.age > max_age:
+                s.close()
 
 @listen()
 async def on_ready():
     print("Bot started")
+
+    asyncio.create_task(timeout_sessions())
 
 @slash_command(description="Launches the Flow Help bot.")
 @slash_option(
@@ -107,7 +174,13 @@ async def on_ready():
     opt_type = OptionType.STRING
 )
 async def flowhelp(ctx: SlashContext, start: str = "index"):
-    await make_flow_message(await ctx.send("Thinking..."), start.strip())
+    while len(sessions) >= int(config['session']['MAX_SESSIONS']):
+        await sessions.pop(list(sessions.keys())[0]).close()
+
+    message = await ctx.send("Thinking...")
+    s = Session(ctx, message)
+    sessions[message.id] = s
+    await s.load(start)
 
 @slash_command(description="ping pong")
 async def ping(ctx: SlashContext):
@@ -118,7 +191,10 @@ async def ping(ctx: SlashContext):
 def main():
     asyncio.run(load_data())
     print("Flow data loaded")
-    bot.start()
+    try:
+        bot.start()
+    except KeyboardInterrupt:
+        asyncio.run(close_sessions())
 
 if __name__ == "__main__":
     main()
